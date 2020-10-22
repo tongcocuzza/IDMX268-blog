6d428fa7f7964378d5dc223229c457ae34f22b2a
Worawalun Cocuzza <wcoc6880@stu.raritanval.edu>
2020-10-22 16:57:34 -0400

    replace the resolve codepen

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,20 +16,6 @@ module.exports = {
   siteMetadata: settings.meta,
   plugins: [
     {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          {
-            resolve:"@weknow/gatsby-remark-codepen",
-            options: {
-              theme: "dark",
-              height: 400
-            }
-          }
-        ]
-      }
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/static/assets/`,
@@ -76,6 +62,13 @@ module.exports = {
               // of HTML entities and their escape value IE: { '}': '&#123;' }
               escapeEntities: {},
             },
+          },
+          {
+            resolve:"@weknow/gatsby-remark-codepen",
+            options: {
+              theme: "dark",
+              height: 400
+            }
           },
         ],
       },
